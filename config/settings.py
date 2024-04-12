389aad57beb3840bd6df6a4aa9855ea35c22dcd6
nsr-g-japan <104160945+nsr-g-japan@users.noreply.github.com>
2024-04-12 13:40:48 +0530

    Initial commit_04

--- config/settings.py
+++ config/settings.py
@@ -115,8 +115,9 @@ USE_TZ = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+EMAIL_PORT = 465
 EMAIL_HOST_USER = 'n.basha@g-japan.com'
 EMAIL_HOST_PASSWORD = 'nbasha2022'
 
